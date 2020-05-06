125dea3371dba29ac81652a8de18801c92183421
juan diego ureña vazquez <juandu89@gmail.com>
2020-05-06 17:04:00 +0900

    add useState and useEffect

--- pages/index.js
+++ pages/index.js
@@ -1,172 +1,52 @@
 import Head from 'next/head'
-const content = {
-  header: `So, how 'bout them Knicks?`,
-  intro: `What are their names? I'm Santa Claus! This opera's as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel. That makes me feel angry! Good news, everyone! I've taught the toaster to feel love!`,
-  list: [
-    `Yes! In your face, Gandhi!`,
-    `So I really am important? How I feel when I'm drunk is correct?`,
-    `Who are those horrible orange men?`,
-  ],
-}
+import { useState, useEffect } from 'react'
 export default function Home() {
+  const [loadingState, updateLoadingState] = useState(true)
+  const [contentState, updateContentState] = useState({
+    header: 'Loading',
+    intro: 'Loading',
+    list: ['Loading', 'Loading', 'Loading'],
+  })
+  const content = {
+    header: `So, how 'bout them Knicks?`,
+    intro: `What are their names? I'm Santa Claus! This opera's as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel. That makes me feel angry! Good news, everyone! I've taught the toaster to feel love!`,
+    list: [
+      `Yes! In your face, Gandhi!`,
+      `So I really am important? How I feel when I'm drunk is correct?`,
+      `Who are those horrible orange men?`,
+    ],
+  }
+  useEffect(() => {
+    setTimeout(() => {
+      updateContentState(content)
+      updateLoadingState(false)
+    }, 2000)
+  }, [])
   return (
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="loading">{content.header}</h1>
-        <p className="loading">{content.intro}</p>
-        <ul className="loading">
-          {content.list.map((item, i) => {
-            return <li key={i}>{item}</li>
-          })}
+      <main className={loadingState ? 'loading' : ''}>
+        <h1>{contentState.header}</h1>
+        <p>{contentState.intro}</p>
+        <ul>
+          {Array.isArray(contentState.list) &&
+            contentState.list.map((item, i) => {
+              return <li key={i}>{item}</li>
+            })}
         </ul>
+        {/* not working for images */}
+        <img
+          src="https://www.w3schools.com/images/lamp.jpg"
+          alt="Lamp"
+          width="42"
+          height="42"
+        />
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-        .loading {
+        .loading h1,
+        .loading p,
+        .loading li,
+        .loading img {
           color: transparent;
           background: linear-gradient(
             100deg,
@@ -185,25 +65,28 @@ export default function Home() {
             background-position: 0 50%;
           }
         }
-        li {
-          margin-bottom: 0.5em;
-        }
       `}</style>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      <style jsx global>
+        {`
+          * {
+            box-sizing: border-box;
+          }
+          body {
+            font-family: sans-serif;
+            padding: 0;
+            margin: 0;
+          }
+          main {
+            max-width: 50em;
+            padding: 0 1em;
+            margin: 0 auto;
+          }
+          li {
+            margin-bottom: 0.5em;
+          }
+        `}
+      </style>
     </div>
   )
 }
